06b9fe71c1cc612d7bab08ab4a0a3d3ffacaf34d
Chase W <thee.wang@outlook.com>
2017-07-18 10:45:38 +0800

    调整文件夹结构

--- app/setup/setup.sh
+++
@@ -1,2 +0,0 @@
-ln -s setup/chatroom.conf /etc/supervisor/conf.d/chatroom.conf
-ln -s setup/chatroom.nginx /etc/nginx/sites-enabled/chatroom
---
+++ setup/setup.sh
@@ -0,0 +1,2 @@
+ln -s /var/www/chatroom/setup/chatroom.conf /etc/supervisor/conf.d/chatroom.conf
+ln -s /var/www/chatroom/setup/chatroom.conf /etc/nginx/sites-enabled/chatroom

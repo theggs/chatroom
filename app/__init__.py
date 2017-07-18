eaa9cff5dd3834f4789e0f604257e2178c46daee
Chase W <thee.wang@outlook.com>
2017-07-18 13:47:26 +0800

    merge into website0

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app(debug=False):
     app.config['SECRET_KEY'] = 'gua'
 
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint, url_prefix='/toys/chatroom')
+    app.register_blueprint(main_blueprint, url_prefix='/websocket/chatroom')
 
     socketio.init_app(app)
 
